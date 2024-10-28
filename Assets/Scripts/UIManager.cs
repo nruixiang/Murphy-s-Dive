1c9e7a876a4bc6732c422a717ad631242ebd41b4
nruixiang <ruidiotxienon@gmail.com>
2024-10-28 14:21:01 +0800

    Reapply "meh"
    This reverts commit d4a38d808afa54855d4257c74af5991e090ef7ff.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,11 +10,15 @@ public class UIManager : MonoBehaviour
     public static float ultCharge = 0;
     float ultReq = 5;
     public static bool ultReady;
+    private bool paused;
+    private bool isPauseMenuLoaded;
 
     // Start is called before the first frame update
     void Start()
     {
         ultReady = false;
+        paused = false;
+        isPauseMenuLoaded = false;
     }
 
     // Update is called once per frame
@@ -32,6 +37,17 @@ public class UIManager : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             HealthManager.health = 3;
         }
+        if (Input.GetKeyDown(KeyCode.Escape)) // Only triggers when the Escape key is pressed down
+        {
+            if (!paused)
+            {
+                PauseGame(); // Pauses the game and loads the pause menu
+            }
+            else
+            {
+                ResumeGame(); // Resumes the game and unloads the pause menu
+            }
+        }
     }
     public void SetUltBarState(float charge, float maxCharge){
         float state = (float)charge;
@@ -40,6 +56,41 @@ public class UIManager : MonoBehaviour
             state = 0f;
         }
         bar.transform.localScale = new Vector3(state, bar.localScale.y, 1f);
+    }
+    private void PauseGame()
+    {
+        if (!isPauseMenuLoaded) // Only load if the pause menu is not already loaded
+        {
+            Time.timeScale = 0;
+            paused = true;
+            isPauseMenuLoaded = true;
+            SceneManager.LoadSceneAsync("PauseMenu", LoadSceneMode.Additive);
+        }
+    }
+
+    private void ResumeGame()
+    {
+        if (isPauseMenuLoaded && IsSceneLoaded("PauseMenu")) // Only unload if the pause menu is loaded
+        {
+            Time.timeScale = 1;
+            paused = false;
+            isPauseMenuLoaded = false;
+            SceneManager.UnloadSceneAsync("PauseMenu");
+        }
+    }
+
+    // Check if a scene is currently loaded
+    private bool IsSceneLoaded(string sceneName)
+    {
+        for (int i = 0; i < SceneManager.sceneCount; i++)
+        {
+            Scene scene = SceneManager.GetSceneAt(i);
+            if (scene.name == sceneName)
+            {
+                return true;
+            }
+        }
+        return false;
     }
     
 }
